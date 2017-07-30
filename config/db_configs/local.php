fc08ea370079f0f3ee83657913ecc677289000ba
Seth Campbell <sethrcamp@gmail.com>
2017-07-30 14:54:01 -0400

    Start of 3 hours

--- config/db_configs/local.php
+++ config/db_configs/local.php
@@ -2,5 +2,5 @@
 $CONFIG['server_name'] = "localhost";
 $CONFIG['server_user'] = "root";
 $CONFIG['server_pass'] = "root";
-$CONFIG['server_db'] = "portfolio";
+$CONFIG['server_db'] = "statusbot";
 $CONFIG['server_port'] = 8889;
