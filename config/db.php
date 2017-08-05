35a43a55b3a2fcd0f7d50be72c0c4257127ffbca
Seth Campbell <sethrcamp@gmail.com>
2017-08-05 01:06:57 -0400

    debugging 5

--- config/db.php
+++ config/db.php
@@ -17,6 +17,7 @@ class DB {
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false
         ];
+        die("whats up");
         $pdo = new PDO($dsn, $user, $pass, $opt);
         return $pdo;
     }
