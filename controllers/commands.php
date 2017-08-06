1dc2e8ab8528ed9cf26a3f7446ddd259d9aae5bd
Seth Campbell <sethrcamp@gmail.com>
2017-08-06 17:17:40 -0400

    lots and lots and lots of stuff

--- controllers/commands.php
+++ controllers/commands.php
@@ -8,7 +8,9 @@ class CommandsController {
         $user = Users::getByName($body['user_name']);
         $user_status = UserStatus::getById(intval($user['id']));
 
-        $words = explode(' ', $body['text']);
+        $words = array_values(array_filter(explode(' ', $body['text']), function($word) {
+            return $word != "";
+        }));
         if(sizeof($words) == 0)
             throw new Exception("You have not provided any parameters");
         $firstWord = strtolower($words[0]);
@@ -29,7 +31,7 @@ class CommandsController {
             $updated_user_status = UserStatus::update(intval($user['id']), $params);
         }
 
-        $message = ["text" => "Your status has been set to ".$status['prefix']."`".$firstWord."`!{".$body['text']."}"];
+        $message = ["text" => "Your status has been set to ".$status['prefix']."`".$firstWord."`!"];
 
 
         if(isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w")
@@ -40,7 +42,9 @@ class CommandsController {
     public static function notifyme($request, $response, $args) {
         $body = $request->getParsedBody();
         $user = Users::getByName($body['user_name']);
-        $words = explode(' ', $body['text']);
+        $words = array_values(array_filter(explode(' ', $body['text']), function($word) {
+            return $word != "";
+        }));
 
         if(sizeof($words) < 1 || sizeof($words) > 3){
             if (isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
@@ -61,13 +65,251 @@ class CommandsController {
         }
 
         $both = false;
+        $start_time = time();
+        $end_time = null;
 
         if(sizeof($words) > 1) {
             $secondWord = $words[1];
             if(strtolower($secondWord) == "both") {
                 $both = true;
             } else {
-                //timing parsing
+                $hours = [];
+                $minutes = [];
+                preg_match('~\d+(?=hrs)|\d+(?=hours)~', $secondWord, $hours);
+                preg_match('~\d+(?=min)|\d+(?=minutes)~', $secondWord, $minutes);
+                if(!(sizeof($minutes) == 0 && sizeof($hours) == 0)) {
+                    if(sizeof($hours) > 1) {
+                        if(isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
+                            $message = ["text" => "Sorry, but you may only define *1* number of hours"];
+                            return $response->withJson($message);
+                        }
+                        throw new Exception("You may only define 1 number of hours");
+                    }
+                    if(sizeof($minutes) > 1) {
+                        if(isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
+                            $message = ["text" => "Sorry, but you may only define *1* number of minutes"];
+                            return $response->withJson($message);
+                        }
+                        throw new Exception("You may only define 1 number of minutes");
+                    }
+                    if(sizeof($minutes) == 1 && sizeof($hours) == 1) {
+                        $end_time = time() + intval($hours[0])*60*60 + intval($minutes[0])*60;
+                    } else if(sizeof($hours) == 1) {
+                        $end_time = time() + intval($hours[0])*60*60;
+                    } else {
+                        $end_time = time() + intval($minutes[0])*60;
+                    }
+                } else {
+                    if(strpos($secondWord, "-") !== false) {
+                        $baseTime = 0;
+                        $addTime = 0;
+                        preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d|\d\d\d\d)(?:@(?:[1-9]|1[012])(?:am|pm))?|(?:(?:[1-9]|1[012])(?:am|pm))|(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)(?:@(?:[1-9]|1[012])(?:am|pm))?)(?=-)~',
+                                    $secondWord,
+                                    $startParam
+                        );
+                        preg_match('~(?<=-)(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d|\d\d\d\d)(?:@(?:[1-9]|1[012])(?:am|pm))?|(?:(?:[1-9]|1[012])(?:am|pm))|(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)(?:@(?:[1-9]|1[012])(?:am|pm))?)~',
+                                    $secondWord,
+                                    $endParam
+                        );
+                        if(sizeof($startParam) != 1 || sizeof($endParam) != 1) {
+                            if(isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
+                                $message = ["text" => "Uh oh! Ranges must be in the format *[time]-[time]*!"];
+                                return $response->withJson($message);
+                            }
+                            throw new Exception("Incorrect range format");
+                        }
+                        if(strpos($startParam[0], "@") !== false){
+                            $dayArray = [];
+                            $dateArray = [];
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)(?=@)~', $startParam[0], $dayArray);
+                            if(sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))(?=@)~', $startParam[0], $dateArray);
+                                $baseTime += strtotime($dateArray[0]);
+                            } else {
+                                if($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now%(24*60*60);
+                                    $baseTime = $now-$secondsPassedToday;
+                                } else {
+                                    $baseTime += strtotime("next ".$dayArray[0]);
+                                }
+                            }
+                            $timeArray = [];
+                            if(strpos($startParam[0], "am") !== false) {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=am)~', $startParam[0], $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0])*60*60);
+                            } else {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=pm)~', $startParam[0], $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? (12*60*60) : ((intval($timeArray[0])+12)*60*60);
+                            }
+                        } else {
+                            $dayArray = [];
+                            $dateArray = [];
+                            $timeArray = [];
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)~', $startParam[0], $dayArray);
+                            if(sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))~', $startParam[0], $dateArray);
+                                if(sizeof($dateArray) == 0){
+                                    if(strpos($startParam[0], "am") !== false) {
+                                        preg_match('~(?:\d\d|\d)(?=am)~', $startParam[0], $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now%(24*60*60);
+                                        $midnightToday = $now-$secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0])*60*60);
+                                        $baseTime = $midnightToday + $hourToday;
+                                    } else {
+                                        preg_match('~(?:\d\d|\d)(?=pm)~', $startParam[0], $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now%(24*60*60);
+                                        $midnightToday = $now-$secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? (12*60*60) : ((intval($timeArray[0])+12)*60*60);
+                                        $baseTime = $midnightToday + $hourToday + (4*60*60);
+                                    }
+                                } else {
+                                    $baseTime += strtotime($dateArray[0]);
+                                }
+                            } else {
+                                if($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now%(24*60*60);
+                                    $baseTime = $now-$secondsPassedToday;
+                                } else {
+                                    $baseTime += strtotime("next ".$dayArray[0]);
+                                }
+                            }
+                        }
+                        $start_time = $baseTime + $addTime;
+                        $baseTime = 0;
+                        $addTime = 0;
+                        if(strpos($endParam[0], "@") !== false){
+                            $dayArray = [];
+                            $dateArray = [];
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)(?=@)~', $endParam[0], $dayArray);
+                            if(sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))(?=@)~', $endParam[0], $dateArray);
+                                $baseTime += strtotime($dateArray[0]);
+                            } else {
+                                if($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now%(24*60*60);
+                                    $baseTime = $now-$secondsPassedToday;
+                                } else {
+                                    $baseTime += strtotime("next ".$dayArray[0]);
+                                }
+                            }
+                            $timeArray = [];
+                            if(strpos($endParam[0], "am") !== false) {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=am)~', $endParam[0], $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0])*60*60);
+                            } else {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=pm)~', $startParam[0], $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? (12*60*60) : ((intval($timeArray[0])+12)*60*60);
+                            }
+                        } else {
+                            $dayArray = [];
+                            $dateArray = [];
+                            $timeArray = [];
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)~', $endParam[0], $dayArray);
+                            if(sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))~', $endParam[0], $dateArray);
+                                if(sizeof($dateArray) == 0){
+                                    if(strpos($endParam[0] , "am") !== false) {
+                                        preg_match('~(?:\d\d|\d)(?=am)~', $endParam[0], $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now%(24*60*60);
+                                        $midnightToday = $now-$secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0])*60*60);
+                                        $baseTime = $midnightToday + $hourToday;
+                                    } else {
+                                        preg_match('~(?:\d\d|\d)(?=pm)~', $endParam[0], $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now%(24*60*60);
+                                        $midnightToday = $now-$secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? (12*60*60) : ((intval($timeArray[0])+12)*60*60);
+                                        $baseTime = $midnightToday + $hourToday + (4*60*60);
+                                    }
+                                } else {
+                                    $baseTime += strtotime($dateArray[0]) + (24*60*60-1);
+                                }
+                            } else {
+                                if($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now%(24*60*60);
+                                    $baseTime = $now-$secondsPassedToday;
+                                } else {
+                                    $baseTime += strtotime("next ".$dayArray[0]) + (24*60*60-1);
+                                }
+                            }
+                        }
+                        $end_time = $baseTime + $addTime;
+                    } else {
+                        $dayArray = [];
+                        $dateArray = [];
+                        $timeArray = [];
+                        if(strpos($secondWord, "@") !== false) {
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)(?=@)~', $secondWord, $dayArray);
+                            if(sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))(?=@)~', $secondWord, $dateArray);
+                                $baseTime = strtotime($dateArray[0]);
+                            } else {
+                                if($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now%(24*60*60);
+                                    $baseTime = $now-$secondsPassedToday;
+                                } else {
+                                    $baseTime = strtotime("next ".$dayArray[0]);
+                                }
+                            }
+                            $timeArray = [];
+                            if(strpos($secondWord, "am") !== false) {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=am)~', $secondWord, $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0])*60*60);
+                            } else {
+                                preg_match('~(?<=@)(?:\d\d|\d)(?=pm)~', $secondWord, $timeArray);
+                                $addTime = intval($timeArray[0]) == 12 ? (12*60*60) : ((intval($timeArray[0])+12)*60*60);
+                            }
+                            $end_time = $baseTime + $addTime;
+                        } else {
+                            preg_match('~(?:sunday|monday|tuesday|wednesday|thursday|friday|saturday|today)~', $secondWord, $dayArray);
+                            if (sizeof($dayArray) == 0) {
+                                preg_match('~(?:(?:0[1-9]|[1-9]|1[012])\/(?:0[1-9]|[1-9]|[12][0-9]|3[01])\/(?:\d\d\d\d|\d\d))~', $secondWord, $dateArray);
+                                if (sizeof($dateArray) == 0) {
+                                    if (strpos($secondWord, "am") !== false) {
+                                        preg_match('~(?:\d\d|\d)(?=am)~', $secondWord, $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now % (24 * 60 * 60);
+                                        $midnightToday = $now - $secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? 0 : (intval($timeArray[0]) * 60 * 60);
+                                        $end_time = $midnightToday + $hourToday;
+                                        if ($end_time < $now)
+                                            $end_time += (24 * 60 * 60);
+                                    } else {
+                                        preg_match('~(?:\d\d|\d)(?=pm)~', $secondWord, $timeArray);
+                                        $now = time();
+                                        $secondsPassedToday = $now % (24 * 60 * 60);
+                                        $midnightToday = $now - $secondsPassedToday;
+                                        $hourToday = intval($timeArray[0]) == 12 ? (12 * 60 * 60) : ((intval($timeArray[0]) + 12) * 60 * 60);
+                                        $end_time = $midnightToday + $hourToday + (4 * 60 * 60);
+                                        if ($end_time < $now)
+                                            $end_time += (24 * 60 * 60);
+                                    }
+                                } else {
+                                    $start_time = strtotime($dateArray[0]);
+                                    $end_time += strtotime($dateArray[0]) + (24 * 60 * 60 - 1);
+                                }
+                            } else {
+                                if ($dayArray[0] == "today") {
+                                    $now = time();
+                                    $secondsPassedToday = $now % (24 * 60 * 60);
+                                    $end_time = $now - $secondsPassedToday + (24 * 60 * 60 - 1);
+                                } else {
+                                    $start_time = strtotime("next " . $dayArray[0]);
+                                    $end_time += strtotime("next " . $dayArray[0]) + (24 * 60 * 60 - 1);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -91,14 +333,27 @@ class CommandsController {
             }
         }
 
+        $data = [
+            "to_user_id" => $user['id'],
+            "from_user_id" => $from_user['id'],
+            "start_time" => $start_time,
+            "end_time" => $end_time,
+            "both_users_free" => $both,
+        ];
+
+        $notification = Notifications::create($data);
+
+        if(isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
+            $message = ["text" => "You will be notified when @".$from_user['slack_handle']." is free!"];
+            return $response->withJson($message);
+        }
+        return $response->withJson($notification);
     }
 
     public static function whereis($request, $response, $args) {
         $body = $request->getParsedBody();
 
-        $words = array_filter(explode(" ", $body['text']), function($word) {
-            return $word != "";
-        });
+        $words =explode(" ", $body['text']);
 
         if(sizeof($words) != 1) {
             if (isset($body['token']) && $body['token'] == "tABWNlxemplvZ2YtVeEMEB5w") {
